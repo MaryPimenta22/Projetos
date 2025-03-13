a9f8d0295e9e06138c73745c2811afa93b17749b
Mary Anne Pimenta <103138288+MaryPimenta22@users.noreply.github.com>
2025-03-13 15:02:31 -0300

    Add files via upload

---
+++ ProjetoComMenu3/ProjetoComMenu3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoComMenu3
+{
+    public partial class Form1 : Form
+    {
+
+        private FilaTADView filaTADView;
+
+        public Form1()
+        {
+            InitializeComponent();
+            filaTADView=new FilaTADView();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (filaTADView == null)
+            {
+                filaTADView=new FilaTADView();
+            }
+            filaTADView.ShowDialog();   
+        }
+    }
+}
